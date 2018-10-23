4dae3b57019b2c27427442c1c91560b1c5436728
forcize <pedro@fusionbreak.com>
2018-10-23 01:54:58 -0400

    Docs: Changed user Credits

--- src/components/App.js
+++ src/components/App.js
@@ -49,7 +49,7 @@ class App extends Component {
             <Container className="mt-5 mb-4">
               <Row>
                 <Col md={12} className="align-self-center">
-                  <div >Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank"  rel="noopener noreferrer">CC 3.0 BY</a></div>
+                  <div >Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank"  rel="noopener noreferrer">CC 3.0 BY</a></div>
                 </Col>
               </Row>
             </Container>
