7bd3136b5e77e843095cd8ae450ef0d8cc105470
forcize <pedro@fusionbreak.com>
2018-10-23 01:52:23 -0400

    Feat: Added new Avatars for every user

--- src/includes/_DATA.js
+++ src/includes/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: 'https://image.flaticon.com/icons/svg/149/149071.svg',
+    avatarURL: 'https://image.flaticon.com/icons/svg/145/145852.svg',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionOne',
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL: 'https://image.flaticon.com/icons/svg/149/149071.svg',
+    avatarURL: 'https://image.flaticon.com/icons/svg/145/145859.svg',
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL: 'https://image.flaticon.com/icons/svg/149/149071.svg',
+    avatarURL: 'https://image.flaticon.com/icons/svg/145/145867.svg',
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
